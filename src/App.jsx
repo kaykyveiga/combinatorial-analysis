8e96723aa2c5d57ea17d576cad88a069182496a8
kaykyveiga <kaykyveiga46@gmail.com>
2023-09-06 19:18:53 -0300

    program structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { FaInstagram, FaEnvelope } from "react-icons/fa"
+import { Outlet } from 'react-router-dom'
 
 import './App.css'
 
@@ -6,7 +8,23 @@ function App() {
 
   return (
     <div className="App">
-
+      <header>
+        <h1>Análise Combinatória</h1>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <p>Entre em contato com o desenvolvedor, clicando aqui:</p>
+        <span className='social-media'>
+          <FaInstagram />
+          <a href="https://www.instagram.com/invites/contact/?i=14v7kz377ljd8&utm_content=4ayc4pr" target='blank'>Instagram</a>
+        </span>
+        <span className='social-media'>
+          <FaEnvelope />
+          <a href="mailto:kaykyveiga46@gmail.com">Email</a>
+        </span>
+      </footer>
     </div>
   )
 }
