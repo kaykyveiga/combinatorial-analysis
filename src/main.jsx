c7be05e4bc12f4a4f7517edc45afdcd3b3feca62
kaykyveiga <kaykyveiga46@gmail.com>
2023-09-06 19:05:21 -0300

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,47 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Home from './pages/Home.jsx';
+import FactorialNumber from './pages/FactorialNumber.jsx';
+import FactorialDivision from './pages/FactorialDivision.jsx';
+import FactorialArrangement from './pages/FactorialArrangement.jsx';
+import FactorialCombination from './pages/FactorialCombination.jsx';
+
+
+import { createHashRouter, RouterProvider } from "react-router-dom"
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "FactorialNumber",
+        element: <FactorialNumber />,
+      },
+      {
+        path : "FactorialDivision",
+        element: <FactorialDivision/>,
+      },
+      {
+        path : "FactorialArrangement",
+        element : <FactorialArrangement/>,
+      },
+      {
+        path : "FactorialCombination",
+        element : <FactorialCombination/>,
+      }
+    ]
+  }
+
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
